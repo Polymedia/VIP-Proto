c312744b0fbb259046feeb0d93a4b5b839088cf7
occash <artiom.shal@gmail.com>
2015-04-09 17:15:51 +0300

    Preload input data

--- src/REditor/main.cpp
+++ src/REditor/main.cpp
@@ -1,13 +1,21 @@
 #include "MainWindow.h"
 #include <QApplication>
+#include <QTableView>
 
 #include <rconsole.h>
+#include <rmodel.h>
+#include <csvmodel.h>
 
 int main(int argc, char *argv[])
-{    
+{
     QApplication a(argc, argv);
 
     RConsole r(true);
+
+    //Load data from csv
+    CsvModel model;
+    model.load("data.csv", ';', true);
+    r.set("input", RObject::fromModel(&model));
 
     MainWindow w(&r);
     w.show();
